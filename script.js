215209cc1f2e07c88b93dd3bb28955d03293cfd9
jonathan <jonathanononivami@gmail.com>
2024-06-07 14:34:19 +0100

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let randomNumber =parseInt((Math.random()*100)+1);
 
 const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#guessfield');
+const guessSlot = document.querySelector('.guesses');
+const remainig = document.querySelector('.last-result');
+const startOver = document.querySelector('.resultpress');
+const submit = document.querySelector('#submit');
