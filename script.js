3c861eebac2924fa6f0912f5fe1d996e096123f4
jonathan <jonathanononivami@gmail.com>
2024-06-07 15:20:03 +0100

    working on javascript

--- script.js
+++ script.js
@@ -5,4 +5,42 @@ const userInput = document.querySelector('#guessfield');
 const guessSlot = document.querySelector('.guesses');
 const remainig = document.querySelector('.last-result');
 const startOver = document.querySelector('.resultpress');
-const submit = document.querySelector('#submit');
+const loworhigh = document.querySelector('.loworhigh');
+
+const p = document.createElement('p');
+
+let previousGuesses = [];
+let numGuesses = 1;
+let playGame = true;
+
+if (playGame){
+    submit.addEventListener('click', function(e){
+       e.preventDefault();
+       const guess = parseInt(userInput.value);
+       validateGuess(guess); 
+    });
+}
+
+function validateGuess(guess){
+
+    if (isNaN(guess)){
+        alert('please enter a valid number');
+    } else if (guess < 1) {
+        alert('please enter a number greater than 1!');
+    } else if (guess > 100){
+       alert('please enter a number less than 500!') 
+    } else{
+       
+        previousGuesses.push(guess);
+
+        if (numGuesses === 11){
+           displayGuesses(guess);
+           displayMessage('Game Over! Number was ${randomNumber}');
+           endGame();  
+        } else {
+            displayGuesses(guess);
+            checkGuess(guess);  
+        }
+    }
+}
+
